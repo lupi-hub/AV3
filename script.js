7aa3c729b69b52e060168c7f3ad0e72abd26b6c9
lupi-hub <lucas.tuleski@escola.pr.gov.br>
2026-08-08 18:36:42 -0300

    1.3

--- script.js
+++ script.js
@@ -18,6 +18,9 @@ if (intro && texto && porcentagemIntro && botaoIniciar) {
 
         porcentagemIntro.innerHTML = progressoIntro + "%";
 
+        intro.style.clipPath =
+    `inset(0 ${100 - progressoIntro}% 0 0)`;
+
 
         if (progressoIntro === 10) {
 
